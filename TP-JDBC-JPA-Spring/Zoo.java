78e719616e2bc148ccede7ec225a42b5983237a1
qaltamore <noctisnoctalobo@gmail.com>
2017-03-02 15:46:21 +0100

    Add files via upload

---
+++ TP-JDBC-JPA-Spring/Zoo.java
@@ -0,0 +1,19 @@
+package fr.testjpa;
+
+/*import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface Zoo extends JpaRepository<Animal, Long>{
+
+	Animal findOne(int id);
+	Animal save(Animal animal);
+}
+*/
+
+public class Zoo {
+	
+	public Zoo() {
+		
+	}
+}
